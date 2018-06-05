3a2f4c2540875e6aadb9ae1523719805bce62a74
smougie <smougie@gmail.com>
2018-06-05 22:19:23 +0200

    Prepared some test plot for project

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+input_values = list(range(1,6))
+plt.plot(input_values, squares, linewidth=5)
+
+# Zdefiniowanie tytułu wykresu i etykiet osi.
+plt.title('Kwadraty liczb', fontsize=24)
+plt.xlabel('Indeks', fontsize=14)
+plt.ylabel('Wartość indeksu', fontsize=14)
+
+# Zdefiniowanie wielkości etykiet.
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
