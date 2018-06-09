d9ee2c36ac92ba87001eefb833b95f9971a4f7c7
smougie <smougie@gmail.com>
2018-06-09 19:50:29 +0200

    Changed number of random walk axis points from 5.000 to 50.000 Plot size changed to fullscreen (20, 10)"

--- rw_visual.py
+++ rw_visual.py
@@ -6,12 +6,15 @@ from random_walk import RandomWalk
 # Utworzenie pętli która będzie tworzyć kolejne błądzenia losowe dopóki użytkownik nie przerwie pętli wpisując 'n'
 while True:
     # Przygotowanie danych błądzenia losowego i wyświetlenie punktów
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Określenie wielkości okna wykresu.
+    plt.figure(figsize=(20, 12))
 
     # Zmienna w której przechowuje listę punktów, które przekażemy do mapowania kolorów.
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
+    plt.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
 
     # Podkreślenie pierwszego i ostatniego punktu bładzenia losowego.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
